92db96c73aa2e9a2ed95238af3cb3e53b2f3cc25
Rahul Sethi <5822355+RamIdeas@users.noreply.github.com>
2024-05-19 22:06:46 +0100

    wip: run unstable_dev fixture tests in a matrix: remote, experimentalDevenvRuntime

--- fixtures/unstable_dev/src/wrangler-dev.mjs
+++ fixtures/unstable_dev/src/wrangler-dev.mjs
@@ -1,11 +1,20 @@
 import { unstable_dev } from "wrangler";
 
-//since the script is invoked from the directory above, need to specify index.js is in src/
-const worker = await unstable_dev("src/index.js");
+for (const options of [
+	{ remote: false, experimentalDevenvRuntime: false },
+	{ remote: false, experimentalDevenvRuntime: true },
+	{ remote: true, experimentalDevenvRuntime: false },
+	{ remote: true, experimentalDevenvRuntime: true },
+]) {
+	//since the script is invoked from the directory above, need to specify index.js is in src/
+	console.log("\n\nBeginning test for:", options);
 
-const resp = await worker.fetch("http://localhost:8787/");
-const text = await resp.text();
+	const worker = await unstable_dev("src/index.js", options);
 
-console.log("Invoked worker: ", text);
+	const resp = await worker.fetch("http://localhost:8787/");
+	const text = await resp.text();
 
-await worker.stop();
+	console.log("Invoked worker: ", text);
+
+	await worker.stop();
+}
